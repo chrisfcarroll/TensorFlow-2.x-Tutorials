e4b22bfd8288783e6e1be2c18486cbd421900d9b
Christopher F Carroll <chris.carroll@unforgettable.me.uk>
2020-10-01 00:46:32 +0100

    ex14p2p.py load data and transform works

--- 14-Pixel2Pixel/ex14p2p.py
+++ 14-Pixel2Pixel/ex14p2p.py
@@ -1,8 +1,11 @@
+from typing import Tuple
+
 import numpy as np
 from numpy import ndarray
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers, models, optimizers, metrics
+from tensorflow.keras.models import Model, Sequential
 from tensorflow.python.data import Dataset
 from matplotlib import pyplot as plt
 import os
@@ -19,28 +22,29 @@ facades_img_height=256
 
 
 def main():
-    generator=Generator()
+    generator : keras.Model =Generator()
     generator.build(input_shape=(batch_size,facades_img_width,facades_img_height,3))
     generator.summary()
-    discriminator=Discriminator()
-    discriminator.build(input_shape=[(batch_size,facades_img_width,facades_img_height,3),(batch_size,facades_img_width,facades_img_height,3)])
+    discriminator: keras.Model =Discriminator()
+    discriminator.build(
+            input_shape=[
+                (batch_size,facades_img_width,facades_img_height,3),
+                (batch_size,facades_img_width,facades_img_height,3)])
     discriminator.summary()
-    gen_optimizer=keras.optimizers.Adam(learning_rate=learning_rate, beta_1=beta_1)
-    disc_optimizer=keras.optimizers.Adam(learning_rate=learning_rate,beta_1=beta_1)
+    generator.optimizer=keras.optimizers.Adam(learning_rate=learning_rate, beta_1=beta_1)
+    discriminator.optimizer=keras.optimizers.Adam(learning_rate=learning_rate,beta_1=beta_1)
+    train_ds, val_ds = get_facade_dataset()
+    print(train_ds.cardinality(),val_ds.cardinality())
+    train(train_ds, generator, discriminator)
 
-    train_ds, val_ds = get_facade_dataset() ; print(train_ds.cardinality(),val_ds.cardinality())
-    train(train_ds)
 
-
-
-def train(train_ds, epochs=100):
+def train(train_ds:Dataset, generator:Model, discriminator:Model, epochs=100):
     print('Training for {} epochs ...'.format(epochs))
     for epoch in range(1,epochs+1):
         start=time.time()
         for step,inputs in enumerate(train_ds):
             input,target=tf.split(inputs,num_or_size_splits=[3,3],axis=3)
             print('input.shape={}, target.shape={}'.format(input.shape, target.shape))
-
 
 
 
@@ -85,56 +89,60 @@ def get_facade_dataset():
     print('Downloading to {}'.format(os.path.dirname(path_to_facadestargz)))
     path_to_train_set = os.path.join(os.path.dirname(path_to_facadestargz), 'facades/train/*.jpg')
     path_to_val_set = os.path.join(os.path.dirname(path_to_facadestargz), 'facades/test/*.jpg')
-    ph_str=tf.keras.backend.placeholder(shape=[],dtype=tf.string)
-    train_ds = tf.data.Dataset.list_files(path_to_train_set)
-    train_ds = train_ds.map(lambda x: tf.py_function(func=load_train_image, inp=[ph_str], Tout=tf.float32)).shuffle(400).batch(1)
+    train_ds1 = tf.data.Dataset.list_files(path_to_train_set)
+    train_ds2= train_ds1.map(tf.io.read_file)
+    train_ds3= train_ds2.map(tf.image.decode_jpeg)
+    train_ds4= train_ds3.map(lambda i: tf.cast(i, tf.float32))
+    train_ds5= train_ds4.map(split_image_in_two)
+    train_ds6= train_ds5.map(lambda d,r:
+                             (tf.image.resize(d, [facades_img_height+30, facades_img_width+30]),
+                              tf.image.resize(r, [facades_img_height+30, facades_img_width+30])))
+    train_ds7 = train_ds6.map(cropped_image_pair)
+    train_ds8 = train_ds7.map(
+            lambda d,r: (d,r) \
+                if np.random.random()>0.5 \
+                else (tf.image.flip_left_right(d),tf.image.flip_left_right(r)))
+    train_ds = train_ds8.map(lambda d,r: (d/127.5 -1, r/127.5 -1) )
+    train_ds = train_ds.map(lambda d,r: tf.concat((d,r),axis=2))
+    train_ds = train_ds.shuffle(400).batch(1)
+
     val_ds = tf.data.Dataset.list_files(path_to_val_set)
-    val_ds = val_ds.map(lambda x: tf.py_function(func=load_val_image, inp=[ph_str], Tout=tf.float32)).shuffle(400).batch(1)
+    val_ds= val_ds.map(tf.io.read_file)
+    val_ds= val_ds.map(tf.image.decode_jpeg)
+    val_ds= val_ds.map(lambda i: tf.cast(i, tf.float32))
+    val_ds = val_ds.map(split_image_in_two)
+    val_ds= val_ds.map(lambda d,r:
+                             (tf.image.resize(d, [facades_img_height+30, facades_img_width+30]),
+                              tf.image.resize(r, [facades_img_height+30, facades_img_width+30])))
+    val_ds = val_ds.map(lambda d,r: (d/127.5 -1, r/127.5 -1) )
+    val_ds = val_ds.map(lambda d,r: tf.concat((d,r),axis=2))
+    val_ds = val_ds.shuffle(400).batch(1)
     return train_ds, val_ds
 
+@tf.function
+def random_jitter_image_pair(drawn:tf.Tensor,real:tf.Tensor):
+    if drawn.shape.rank>0:
+        drawn = tf.image.resize(drawn, [facades_img_height+30, facades_img_width+30])
+        real = tf.image.resize(real, [facades_img_height+30, facades_img_width+30])
+        drawn, real = cropped_image_pair(drawn, real)
+        if np.random.random() > 0.5:
+            drawn = tf.image.flip_left_right(drawn)
+            real = tf.image.flip_left_right(real)
+    return drawn,real
 
-def load_train_image(image_filename):
-    """
-    load and preprocess images
-    """
-    drawn, real = half_images_from_filename(image_filename)
-    drawn, real= random_jitter_image_pair(drawn, real)
-    drawn, real = drawn/127.5- 1, real/127.5 - 1
-    return tf.concat([drawn,real],axis=2)
-
-
-def load_val_image(image_filename):
-    """
-    load and preprocess images
-    """
-    drawn, real = half_images_from_filename(image_filename)
-    drawn=tf.image.resize(drawn, size=[facades_img_height,facades_img_width])
-    real=tf.image.resize(real, size=[facades_img_height,facades_img_width])
-    drawn, real = drawn/127.5- 1, real/127.5 - 1
-    return tf.concat([drawn,real],axis=2)
-
-
-def half_images_from_filename(image_filename):
-    images = tf.io.read_file(image_filename[0])
-    images = tf.image.decode_jpeg(images)
-    images = tf.cast(images, tf.float32)
-    print('filename {} shape {}'.format(image_filename[0], images.shape))
-    actual_width = images.shape[1] // 2
-    drawn = images[:, actual_width:, :]  # right half
-    real = images[:, :actual_width, :]  # left half
-    return drawn, real
-
-
-def random_jitter_image_pair(drawn,real):
-    drawn = tf.image.resize(drawn, [facades_img_height+30, facades_img_width+30])
-    real = tf.image.resize(real, [facades_img_height+30, facades_img_width+30])
+@tf.function
+def cropped_image_pair(drawn, real):
     stacked = tf.stack([drawn, real], axis=0)
     cropped = tf.image.random_crop(stacked, size=[2, facades_img_height, facades_img_width, 3])
     drawn, real = cropped[0], cropped[1]
-    if np.random.random() > 0.5:
-        drawn = tf.image.flip_left_right(drawn)
-        real = tf.image.flip_left_right(real)
-    return real,drawn
+    return drawn, real
+
+
+@tf.function
+def split_image_in_two(image_pair, halfwidth=facades_img_width // 2):
+    drawn = image_pair[:, halfwidth:, :]  # right half
+    real = image_pair[:, :halfwidth, :]  # left half
+    return drawn, real
 
 
 if __name__ == '__main__':
